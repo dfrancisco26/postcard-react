71918d1542a12afc89e7fbf355616432b41f7d16
David Francisco <dfrancisco2677@gmail.com>
2022-06-16 21:43:20 -0700

    Built some components and state tracked in parent.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import CityNameInput from './CityNameInput';
 import City from './City';
 
 function App() {
+  const [skylineId, setSkylineId] = useState(1);
+  const [waterfrontId, setWaterfrontId] = useState(1);
+  const [castleId, setCastleId] = useState(1);
+  const [cityName, setCityName] = useState('Bald Knob');
+  const [sloganArr, setSloganArr] = useState(['Middle of no hair']);
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
   // you'll need to track a city name, which starts as the city name of your choice.
@@ -16,14 +21,20 @@ function App() {
 
   return (
     <div className="App">
+      <City skylineId={skylineId} waterfrontId={waterfrontId} castleId={castleId} />
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to beautiful {cityName}
       </h1>
       <div className='bottom'>
+        <CityNameInput setCityName = {setCityName} />
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
         <section className='dropdowns'>
+          <WaterfrontDropdown setWaterfrontId = {setSkylineId} />
+          <SkylineDropdown setSkylineId = {setSkylineId} />
+          <CastleDropdown setCastleId = {setCastleId} />          
           {/* 
           render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
           
--- src/City.js
+++ src/City.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function City(props) {
+export default function City(skylineId, waterfrontId, castleId) {
   return (
     <div className="city">
-      {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
+      
+      {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
       {/* use these 3 ids in img tags load approriate images that live in the public directory */}
     </div>
   );
