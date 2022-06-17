001187af83888e138a1901018ecf65319d6a9684
David Francisco <dfrancisco2677@gmail.com>
2022-06-17 00:04:45 -0700

    Controlled input.

--- src/SloganForm.js
+++ src/SloganForm.js
@@ -22,7 +22,7 @@ export default function SloganForm({ setSloganArr, sloganArr }) {
         Add a slogan to the list!
         {/* on change, set the sloganInput in state to be the input value */}
         {/* also, weirdly, set the value of this input to the sloganInput tracked in state to make this a 'controlled' form input */}
-        <input onChange={(e) => setSloganInput(e.target.value)}/>
+        <input value={sloganInput} onChange={(e) => setSloganInput(e.target.value)}/>
         <button>Submit</button>
       </form>
     </section>
